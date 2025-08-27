6f81c1721f75cea97b4a6ed35f41dad9b13f7290
Val <val.cb@ciencias.unam.mx>
2025-08-27 15:05:24 -0600

    ejercicios

--- Ejercicios/CrearArchivo.java
+++ Ejercicios/CrearArchivo.java
@@ -7,7 +7,13 @@ public class CrearArchivo {
     public static void main(String[] args) {
         try {
             File archivo = new File("ejemplo.txt");
-            archivo.createNewFile();
+            if (archivo.createNewFile())
+                System.Out.println("Archivo creado: " + archivo.getName())
+            else
+                System.Out.println("El archivo ya existe.");
+            FileWriter escritor = new FileWriter(archivo);
+            escritor.write("stuff");
+            escritor.close();
         } catch (IOException e) {
             System.out.println("Ocurrió un error.");
             e.printStackTrace();
---
+++ Ejercicios/LeerArchivo.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LeerArchivo {
+	public static void main (String[] args) {
+		try {
+			File archivo = new File("ejemplo.txt");
+			Scanner lector = new Scanner(archivo) ;
+			while (lector.hasNextLine()) {
+				String linea = lector.nextLine();
+				System.out.print(linea);
+			}
+			lector.close();
+		} catch (FileNotFoundException e) {
+			System.out.print("cannot");
+		}
+	}
+}
